3f3586bb848ebf620fca011ad241d9ea987ee0c1
Anyamanee Phengthong <unyamanee1a@gmail.com>
2024-09-05 17:00:56 +0700

    feat: write the controller for new and create function

--- app/controllers/register_form_controller.rb
+++ app/controllers/register_form_controller.rb
@@ -1,2 +1,14 @@
 class RegisterFormController < ApplicationController
+  def new
+    @register = RegisterForm.new
+  end
+
+  def create
+    @register = RegisterForm.new(userData)
+    if @register.save
+      redirect_to root_path
+    else
+        render :new
+    end 
+  end
 end
